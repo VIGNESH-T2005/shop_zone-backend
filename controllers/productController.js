050769839e012afc570c74087c0aa8251304a4d3
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-14 20:49:22 +0530

    cud  product control

--- controllers/productController.js
+++ controllers/productController.js
@@ -25,7 +25,89 @@ const getProductById = async (req, res) => {
   }
 }
 
+
+const createProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      image,
+      countInStock,
+      rating,
+    } = req.body
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      category,
+      image,
+      countInStock,
+      rating,
+    })
+
+    res.status(201).json(product)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+
+const updateProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      image,
+      countInStock,
+      rating,
+    } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      product.name = name || product.name
+      product.description = description || product.description
+      product.price = price || product.price
+      product.category = category || product.category
+      product.image = image || product.image
+      product.countInStock = countInStock || product.countInStock
+      product.rating = rating || product.rating
+
+      const updatedProduct = await product.save()
+      res.json(updatedProduct)
+    } else {
+      res.status(404).json({ message: 'Product not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      await product.deleteOne()
+      res.json({ message: 'Product removed' })
+    } else {
+      res.status(404).json({ message: 'Product not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
 export {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 }
