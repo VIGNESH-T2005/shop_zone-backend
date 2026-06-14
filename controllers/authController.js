7eec986f4f91a7f72b89da0776a49645616f7d0a
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-14 20:41:39 +0530

    login user auth control

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,6 +48,31 @@ const registerUser = async (req, res) => {
 }
 
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    // Find user by email
+    const user = await User.findOne({ email })
+
+    // Check user and password
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
 
 
-export { registerUser }
+
+
+export { registerUser, loginUser }
