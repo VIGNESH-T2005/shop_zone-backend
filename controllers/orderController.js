8b0648103fcde48eae86c58acd4d8b46ff090139
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-14 20:53:33 +0530

    get by id and update  order control

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -29,7 +29,6 @@ const createOrder = async (req, res) => {
   }
 }
 
-
 const getMyOrders = async (req, res) => {
   try {
     const orders = await Order.find({ user: req.user._id })
@@ -39,7 +38,47 @@ const getMyOrders = async (req, res) => {
   }
 }
 
+
+const getOrderById = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    )
+
+    if (order) {
+      res.json(order)
+    } else {
+      res.status(404).json({ message: 'Order not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+
+const updateOrderToDelivered = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+      order.isDelivered = true
+      order.deliveredAt = Date.now()
+
+      const updatedOrder = await order.save()
+      res.json(updatedOrder)
+    } else {
+      res.status(404).json({ message: 'Order not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+
 export {
   createOrder,
-  getMyOrders
+  getMyOrders,
+  getOrderById,
+  updateOrderToDelivered
 }
