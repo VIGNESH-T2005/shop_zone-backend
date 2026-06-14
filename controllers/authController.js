3a9c1f235975f5cc13f2e4e16d1036522c63f43e
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-14 20:42:36 +0530

    get user profiel  auth control

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,6 +73,23 @@ const loginUser = async (req, res) => {
 }
 
 
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id)
 
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      })
+    } else {
+      res.status(404).json({ message: 'User not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
 
-export { registerUser, loginUser }
+export { registerUser, loginUser, getUserProfile }
